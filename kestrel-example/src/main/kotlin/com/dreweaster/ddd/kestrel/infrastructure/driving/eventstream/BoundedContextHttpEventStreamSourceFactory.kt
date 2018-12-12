1bd33551b0a76be7912fc484ae831f12cbba1a41
Andrew Easter <andrew.easter@gmail.com>
2018-12-12 16:59:05 +0000

    shifted bounded context http event stream source factory to be configured using DSL pattern

--- kestrel-example/src/main/kotlin/com/dreweaster/ddd/kestrel/infrastructure/driving/eventstream/BoundedContextHttpEventStreamSourceFactory.kt
+++ kestrel-example/src/main/kotlin/com/dreweaster/ddd/kestrel/infrastructure/driving/eventstream/BoundedContextHttpEventStreamSourceFactory.kt
@@ -1,16 +1,22 @@
 package com.dreweaster.ddd.kestrel.infrastructure.driving.eventstream
 
 import com.dreweaster.ddd.kestrel.application.eventstream.BoundedContextName
+import com.dreweaster.ddd.kestrel.application.eventstream.EventMetadata
+import com.dreweaster.ddd.kestrel.application.eventstream.StatelessEventConsumer
 import com.dreweaster.ddd.kestrel.application.job.JobManager
+import com.dreweaster.ddd.kestrel.domain.DomainEvent
+import com.dreweaster.ddd.kestrel.domain.DomainEventTag
+import com.dreweaster.ddd.kestrel.domain.aggregates.user.FailedLoginAttemptsIncremented
 import com.dreweaster.ddd.kestrel.infrastructure.http.eventstream.consumer.BoundedContextHttpEventStreamSource
 import com.dreweaster.ddd.kestrel.infrastructure.http.eventstream.consumer.BoundedContextHttpEventStreamSourceConfiguration
 import com.dreweaster.ddd.kestrel.infrastructure.http.eventstream.consumer.HttpJsonEventMapper
 import com.dreweaster.ddd.kestrel.infrastructure.http.eventstream.consumer.offset.OffsetManager
+import com.google.gson.JsonObject
 import org.asynchttpclient.AsyncHttpClient
 
 abstract class BoundedContextHttpEventStreamSourceFactory(val name: BoundedContextName) {
 
-    abstract val mappers: List<HttpJsonEventMapper<*>>
+    protected abstract val mappers: EventMappers
 
     fun createHttpEventStreamSource(
             httpClient: AsyncHttpClient,
@@ -23,7 +29,37 @@ abstract class BoundedContextHttpEventStreamSourceFactory(val name: BoundedConte
                 configuration = configuration,
                 jobManager = jobManager,
                 offsetManager = offsetManager,
-                eventMappers = mappers
+                eventMappers = mappers.build()
         )
+    }
+
+    class EventMappers {
+
+        var mappersList: List<HttpJsonEventMapper<*>> = emptyList()
+
+        fun tag(tagName: String, init: Tag.() -> Unit): Tag {
+            val tag = Tag(DomainEventTag(tagName))
+            tag.init()
+            return tag
+        }
+
+        fun build(): List<HttpJsonEventMapper<*>> = mappersList
+
+        inner class Tag(val tag: DomainEventTag) {
+            inline fun <reified E: DomainEvent> event(sourceEventType: String, noinline handler: (JsonObject) -> E) {
+                mappersList += HttpJsonEventMapper(
+                    sourceEventType = sourceEventType,
+                    sourceEventTag = tag,
+                    targetEventClass = E::class,
+                    map = handler
+                )
+            }
+        }
+    }
+
+    fun eventMappers(init: EventMappers.() -> Unit): EventMappers {
+        val mappers = EventMappers()
+        mappers.init()
+        return mappers
     }
 }
--- kestrel-example/src/main/kotlin/com/dreweaster/ddd/kestrel/infrastructure/driving/eventstream/UserContextHttpEventStreamSourceFactory.kt
+++ kestrel-example/src/main/kotlin/com/dreweaster/ddd/kestrel/infrastructure/driving/eventstream/UserContextHttpEventStreamSourceFactory.kt
@@ -1,55 +1,22 @@
 package com.dreweaster.ddd.kestrel.infrastructure.driving.eventstream
 
 import com.dreweaster.ddd.kestrel.application.consumers.BoundedContexts.UserContext
-import com.dreweaster.ddd.kestrel.domain.DomainEventTag
 import com.dreweaster.ddd.kestrel.domain.aggregates.user.*
 import com.dreweaster.ddd.kestrel.infrastructure.driven.serialisation.user.PasswordChangedMapper
 import com.dreweaster.ddd.kestrel.infrastructure.driven.serialisation.user.UserRegisteredMapper
 import com.dreweaster.ddd.kestrel.infrastructure.driven.serialisation.user.UsernameChangedMapper
-import com.dreweaster.ddd.kestrel.infrastructure.http.eventstream.consumer.HttpJsonEventMapper
 
 object UserContextHttpEventStreamSourceFactory: BoundedContextHttpEventStreamSourceFactory(UserContext) {
 
-    private val userEventTag = DomainEventTag("user-event")
+    override val mappers = eventMappers {
 
-    private val userRegisteredMapper = HttpJsonEventMapper(
-            sourceEventType = "com.dreweaster.ddd.kestrel.domain.aggregates.user.UserRegistered",
-            sourceEventTag = userEventTag,
-            targetEventClass = UserRegistered::class,
-            map = UserRegisteredMapper.deserialiser)
-
-    private val passwordChangedMapper = HttpJsonEventMapper(
-            sourceEventType = "com.dreweaster.ddd.kestrel.domain.aggregates.user.PasswordChanged",
-            sourceEventTag = userEventTag,
-            targetEventClass = PasswordChanged::class,
-            map = PasswordChangedMapper.deserialiser)
-
-    private val usernameChangedMapper = HttpJsonEventMapper(
-            sourceEventType = "com.dreweaster.ddd.kestrel.domain.aggregates.user.UsernameChanged",
-            sourceEventTag = userEventTag,
-            targetEventClass = UsernameChanged::class,
-            map = UsernameChangedMapper.deserialiser)
-
-    private val userLockedMapper = HttpJsonEventMapper(
-            sourceEventType = "com.dreweaster.ddd.kestrel.domain.aggregates.user.UserLocked",
-            sourceEventTag = userEventTag,
-            targetEventClass = UserLocked::class) { UserLocked }
-
-    private val userUnlockedMapper = HttpJsonEventMapper(
-            sourceEventType = "com.dreweaster.ddd.kestrel.domain.aggregates.user.UserUnlocked",
-            sourceEventTag = userEventTag,
-            targetEventClass = UserUnlocked::class) { UserUnlocked }
-
-    private val failedLoginAttemptsIncrementedMapper = HttpJsonEventMapper(
-            sourceEventType = "com.dreweaster.ddd.kestrel.domain.aggregates.user.FailedLoginAttemptsIncremented",
-            sourceEventTag = userEventTag,
-            targetEventClass = FailedLoginAttemptsIncremented::class) { FailedLoginAttemptsIncremented }
-
-    override val mappers = listOf(
-        userRegisteredMapper,
-        passwordChangedMapper,
-        usernameChangedMapper,
-        userLockedMapper,
-        userUnlockedMapper,
-        failedLoginAttemptsIncrementedMapper)
+        tag("user-event") {
+            event<UserRegistered>("com.dreweaster.ddd.kestrel.domain.aggregates.user.UserRegistered", UserRegisteredMapper.deserialiser)
+            event<PasswordChanged>("com.dreweaster.ddd.kestrel.domain.aggregates.user.PasswordChanged", PasswordChangedMapper.deserialiser)
+            event<UsernameChanged>("com.dreweaster.ddd.kestrel.domain.aggregates.user.UsernameChanged", UsernameChangedMapper.deserialiser)
+            event<UserLocked>("com.dreweaster.ddd.kestrel.domain.aggregates.user.UserLocked") { UserLocked }
+            event<UserUnlocked>("com.dreweaster.ddd.kestrel.domain.aggregates.user.UserUnlocked") { UserUnlocked}
+            event<FailedLoginAttemptsIncremented>("com.dreweaster.ddd.kestrel.domain.aggregates.user.FailedLoginAttemptsIncremented") { FailedLoginAttemptsIncremented }
+        }
+    }
 }
